72516e520b32737e65c37311cbe93604354f18b8
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-16 19:31:52 +0530

    selected a movie

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -51,26 +51,147 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [query, setQuery] = useState("inception");
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+
+  const KEY = "24a2b866";
+
+  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //   .then((res) => res.json())
+  //   .then((data) => setMovies(data.Search));
+  // when we check out the network tab so you see that it's basically running an infinite number of requests here so it keeps going and it never really stops so every second our app is firing off multiple fetch requests to this API which ofcourse is a really bad idea so why do you think all these fetch requests are being fired off well the reason is that setting the state here in the render logic will then immediately cause the component to rerender itself again so that's just how state works however as the components is re-rendered the function here ofcourse is executed again which then will fetch again which in turn will set the movies again as well and then this whole thing starts over and over again so as the state is that the component is re-rendered again, which then will fetch again which will set the movies again and so this really is an infinite loop of state setting and then the component re-rendering and so this is the reason why it is really not allowed to set state in render logic.
+
+  // setWatched([]);
+  // same problem, now we do get a real error, here we are now reloading all the time again the data from the API but what matters here is that we got the error of too many re-renders and that's because of above state setting, so if we're really setting the state here in the top level, even without being inside a then handler then immediately react will complain that there are too many renders, which means that we again entered that infinite loop where updating state will cause the component to render which will cost the state to be set and so on to infinity.
+
+  // useffect to the rescue:-
+  // useEffect(function () {
+  //   fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //     .then((res) => res.json())
+  //     .then((data) => setMovies(data.Search));
+  // }, []);
+  // now we have no more infinite loops here and no more infinite requests to our API, so now our effect is only running as the component mounts, so again we used the use effect hook to register an effect and that effect is the function inside of it which contains the side effect that we want to register and basically register means that we want this code here not to run as the component renders but actually after it has been painted onto the screen and so that's exactly what useeffect does so while before the code was executed while the component was rendering so while the function was being executed, now this effect will actually be executed after render and that's a lot better then as a second argument we passed this empty array here into useeffect and so this means that this effect will only be executed as the component first mounts.
+
+  useEffect(function () {
+    console.log("A: after initial render");
+  }, []);
+
+  useEffect(function () {
+    console.log("B: after every render");
+  });
+
+  useEffect(
+    function () {
+      console.log("D");
+    },
+    [query]
+  );
+
+  console.log("C: during render");
+  // so why did we get C first, even though it appears later here in code, well the reason is that as we just discuessed before effects actually only run after the browser paint while the render logic itself runs well as the name says during render and so then it makes sense that ofcourse this console.log() here is executed first so during the render of this component and then we have A and B which comes form above two effects and so A is rendered first simply because it appears first in the code
+
+  // and when we type on search bar something, so we update the state here which is the query state and as a result the component was re-rendered and then just like before C was executed and so therefore we see the letter C first and then after that we also have a B log so B has no dependency array which remember basically means that this effect is synchronized with everything and so therefore it needs to run on every render while the other effect A is synchronized with no variables at all which is the meaning of this empty array and therefore this effect was not executed as the component was re-rendered with the query state
+
+  // now the D will be executed every time the query state will change, so now D useeffect is synchronized with the query state
+
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (selectedId === id ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  const tempQuery = "interstellar";
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+
+          if (!res.ok)
+            throw new Error("something went wrong with fetching movies");
+
+          const data = await res.json();
+
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          // console.log(data);
+
+          // console.log(movies);
+          // now why are we getting empty array: after the state has been set in the setMovies that doesn't means data fetchs immediately so this will happen after this function here is called and so right now in console.log we have a stale state which basically means that we still have the old value as the state was before and in this case before it was just an empty array so our initial state
+        } catch (err) {
+          // console.err(err.message);
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+    },
+    [query]
+  );
 
   return (
     <>
       <NavBar>
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
+          {error && <ErrorMessage message={error} />}
         </Box>
 
         <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          )}
         </Box>
       </Main>
     </>
+  );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔</span> {message}
+    </p>
   );
 }
 
@@ -92,9 +213,7 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState("");
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"
@@ -153,19 +272,19 @@ function Box({ children }) {
 //   );
 // }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
+        <Movie movie={movie} onSelectMovie={onSelectMovie} key={movie.imdbID} />
       ))}
     </ul>
   );
 }
 
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
   return (
-    <li>
+    <li onClick={() => onSelectMovie(movie.imdbID)}>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
@@ -175,6 +294,17 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
+  );
+}
+
+function MovieDetails({ selectedId, onCloseMovie }) {
+  return (
+    <div className="details">
+      <button className="btn-back" onClick={onCloseMovie}>
+        &larr;
+      </button>
+      {selectedId}
+    </div>
   );
 }
 
