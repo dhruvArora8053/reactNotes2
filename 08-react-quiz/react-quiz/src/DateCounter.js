4473765a18b5b156ab7d4db6d4146a86b429ee30
Dhruv Arora <dhruvarora.code@gmail.com>
2023-12-05 13:03:11 +0530

    implemented usereducer hook

--- 08-react-quiz/react-quiz/src/DateCounter.js
+++ 08-react-quiz/react-quiz/src/DateCounter.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+import { act } from "react-dom/test-utils";
+
+// function reducer(state, action) {
+//   console.log(state, action);
+//   // as we print our output we get 0 1, meaining the state is 0 and the action is 1 so why is that, well it is because the reducer function gets access to the current state which right now is 0 so it is the initial state then it also gets access to the action, which right now is simpy 1 which we passed into the dispatch function so this dispatch in 1 is basically goint to become the action in the reducer and so the idea of the reducer is to take these tow things so the current state+ the action and based on that return the next state
+//   return state + action;
+// }
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+  // this dispatch function can also be used to update the state
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,17 +26,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +47,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
