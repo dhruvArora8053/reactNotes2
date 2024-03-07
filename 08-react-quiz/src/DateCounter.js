e3aa76bc1f7835a3e1ed6b40d43d5d7559899eaa
Dhruv Arora <dhruvarora.code@gmail.com>
2024-03-07 22:16:21 +0530

    new files added

--- 08-react-quiz/src/DateCounter.js
+++ 08-react-quiz/src/DateCounter.js
@@ -1,5 +1,4 @@
-import { useReducer } from "react";
-import { act } from "react-dom/test-utils";
+import { useReducer, useState } from "react";
 
 const initialState = { count: 0, step: 1 };
 
@@ -8,23 +7,17 @@ function reducer(state, action) {
   // if (action.type === "inc") return state + 1;
   // if (action.type === "dec") return state - 1;
   // if (action.type === "setCount") return action.payload;
-
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
-
     case "inc":
       return { ...state, count: state.count + state.step };
-
     case "setCount":
       return { ...state, count: action.payload };
-
     case "setStep":
       return { ...state, step: action.payload };
-
     case "reset":
       return initialState;
-
     default:
       throw new Error("Unknown action");
   }
---
+++ 09-worldwise/worldwise/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  const x = 23;
+
+  return <div>WordlWise</div>;
+}
+
+export default App;
