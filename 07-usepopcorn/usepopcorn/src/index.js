e4ed9083d14a40bb6a029a68d12ffd346e50097e
Dhruv Arora <dhruvarora.code@gmail.com>
2024-05-21 19:08:47 +0530

    implemented prop types

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
 
